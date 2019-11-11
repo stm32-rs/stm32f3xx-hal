5d42b307cadef45271d55e9c18b4d18db58427a2
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2019-11-10 16:13:26 -0800

    Move helper to turn TIM8 into PWM channels from the timer module to the pwm module.

--- src/pwm.rs
+++ src/pwm.rs
@@ -4,6 +4,7 @@ use embedded_hal::PwmPin;
 use super::gpio::{AF4, AF10};
 use super::gpio::gpioc::{PC8};
 use super::gpio::gpiob::{PB9};
+use crate::rcc::{Clocks, APB1, APB2};
 
 //pub struct Tim1Ch1 {}
 //pub struct Tim1Ch2 {}
@@ -26,6 +27,59 @@ pub struct WithPins {}
 pub struct PwmChannel<X, T> {
     pub(crate) timx_chx: PhantomData<X>,
     pub(crate) pin_status: PhantomData<T>, // TODO: use PhantomData
+}
+
+pub fn tim8(tim: TIM8, res: u16, freq: u16, apb2: &mut APB2, clocks: &Clocks) -> PwmChannel<Tim8Ch3, NoPins> {
+    // Power the timer
+    apb2.enr().modify(|_, w| w.tim8en().set_bit());
+
+    // enable auto reload preloader
+    tim.cr1.write(|w| w.arpe().set_bit());
+
+    // Set the "resolution" of the duty cycle (ticks before restarting at 0)
+    tim.arr.write(|w| w.arr().bits(res));
+    // TODO: Use Hertz?
+    // Set the pre-scaler
+    tim.psc.write(|w| w.psc().bits(clocks.pclk2().0 as u16 / (res * freq)));
+
+    // Macro friendly for later
+    if true {
+        // Make the settings reload immediately for TIM1/8
+        tim.egr.write(|w| w.ug().set_bit());
+    }
+
+    tim.smcr.write(|w| w); // Reset the slave/master config
+    tim.cr2.write(|w| w); // reset
+    tim.ccmr1_output().write(|w| w
+        // Select PWM Mode 1 for CH1/CH2
+        .oc1m().bits(0b0110)
+        .oc2m().bits(0b0110)
+        // set pre-load enable so that updates to the duty cycle
+        // propagate but _not_ in the middle of a cycle.
+        .oc1pe().set_bit()
+        .oc2pe().set_bit()
+    );
+    tim.ccmr2_output().write(|w| w
+        // Select PWM Mode 1 for CH3/CH4
+        .oc3m().bits(0b0110)
+        .oc4m().bits(0b0110)
+        // set pre-load enable so that updates to the duty cycle
+        // propagate but _not_ in the middle of a cycle.
+        .oc3pe().set_bit()
+        .oc4pe().set_bit()
+    );
+
+    // Macro friendly for later
+    if true {
+        // Enable outputs (STM32 Break Timer Specific)
+        tim.bdtr.write(|w| w.moe().set_bit());
+    }
+
+    // Enable the Timer
+    tim.cr1.modify(|_, w| w.cen().set_bit());
+
+    // TODO: This should return all four channels
+    PwmChannel { timx_chx: PhantomData, pin_status: PhantomData }
 }
 
 macro_rules! pwm_channel_pin {
