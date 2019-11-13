35f4a7c99d49fb3277524d7c8f4c87a43449673c
Sh3Rm4n <f.vioel@googlemail.com>
2019-11-13 21:19:13 +0100

    Add toggle example

---
+++ examples/toggle.rs
@@ -0,0 +1,41 @@
+//! Uses the StatefulOutputPin embedded_hal trait to toggle the pin
+//! On the stm32 discovery board this is the "south" led
+//! Target board: STM32F3DISCOVERY
+
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+extern crate panic_semihosting;
+
+use cortex_m_rt::entry;
+use stm32f3xx_hal::prelude::*;
+use stm32f3xx_hal::stm32;
+
+#[entry]
+fn main() -> ! {
+    let dp = stm32::Peripherals::take().unwrap();
+
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+
+    let mut led = gpioe
+        .pe13
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+    led.set_low().unwrap();
+
+    loop {
+        led.toggle().unwrap();
+        cortex_m::asm::delay(8_000_000);
+        // Toggle by hand.
+        // Uses `StatefulOutputPin` instead of `ToggleableOutputPin`.
+        // Logically it is the same.
+        if led.is_set_low().unwrap() {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
+        }
+        cortex_m::asm::delay(8_000_000);
+    }
+}
