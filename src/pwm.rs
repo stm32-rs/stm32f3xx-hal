3c114e1ee1ed3a5cfcca38a9403e91909f5d6fb0
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2019-11-26 08:47:04 -0800

    Enable stm32f373 channel/pin combinations for TIM5.

--- src/pwm.rs
+++ src/pwm.rs
@@ -2,9 +2,9 @@ use core::marker::PhantomData;
 use crate::stm32::{TIM2, TIM15, TIM16, TIM17};
 use embedded_hal::PwmPin;
 use crate::gpio::{AF1, AF2, AF3, AF4, AF5, AF9, AF10};
-use crate::gpio::gpioa::{PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA9, PA10, PA12, PA13, PA14, PA15};
+use crate::gpio::gpioa::{PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9, PA10, PA11, PA12, PA13, PA14, PA15};
 use crate::gpio::gpiob::{PB0, PB1, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB14, PB15};
-use crate::gpio::gpioc::{PC6, PC7, PC8, PC9, PC10, PC11, PC12};
+use crate::gpio::gpioc::{PC0, PC1, PC2, PC3, PC6, PC7, PC8, PC9, PC10, PC11, PC12};
 use crate::gpio::gpiod::{PD1, PD3, PD4, PD6, PD7};
 use crate::gpio::gpioe::{PE0, PE1, PE4, PE6, PE7, PE8, PE9};
 use crate::gpio::gpiof::{PF9, PF10};
@@ -354,6 +354,59 @@ pwm_channel_pin!(WithPins, TIM3, TIM3_CH2, output_to_pb0, PB0, AF10, ccmr1_outpu
 
 #[cfg(any(feature = "stm32f373", feature = "stm32f378"))]
 pwm_channel_pin!(WithPins, TIM3, TIM3_CH3, output_to_pb6, PB6, AF10, ccmr2_output, oc3m, oc3pe);
+
+
+// TIM5
+
+#[cfg(feature = "stm32f373")]
+macro_rules! tim5 {
+    () => {
+        use crate::stm32::TIM5;
+
+        pub struct TIM5_CH1 {}
+        pub struct TIM5_CH2 {}
+        pub struct TIM5_CH3 {}
+        pub struct TIM5_CH4 {}
+
+        pwm_timer_basic!(
+            tim5,
+            TIM5,
+            u32,
+            apb1enr,
+            pclk1,
+            tim5en,
+            [TIM5_CH1,TIM5_CH2,TIM5_CH3,TIM5_CH4],
+            [PwmChannel,PwmChannel,PwmChannel,PwmChannel]
+        );
+
+        // Channels
+        pwm_pin_for_pwm_channel!(TIM5, TIM5_CH1, u32, cc1e, ccr1, ccr);
+        pwm_pin_for_pwm_channel!(TIM5, TIM5_CH2, u32, cc2e, ccr2, ccr);
+        pwm_pin_for_pwm_channel!(TIM5, TIM5_CH3, u32, cc3e, ccr3, ccr);
+        pwm_pin_for_pwm_channel!(TIM5, TIM5_CH4, u32, cc4e, ccr4, ccr);
+
+        // Pins
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH1, output_to_pa0, PA0, AF2, ccmr1_output, oc1m, oc1pe);
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH1, output_to_pa8, PA8, AF2, ccmr1_output, oc1m, oc1pe);
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH1, output_to_pc0, PC0, AF2, ccmr1_output, oc1m, oc1pe);
+
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH2, output_to_pa1, PA1, AF2, ccmr1_output, oc2m, oc2pe);
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH2, output_to_pa11, PA11, AF2, ccmr1_output, oc2m, oc2pe);
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH2, output_to_pc1, PC1, AF2, ccmr1_output, oc2m, oc2pe);
+
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH3, output_to_pa2, PA2, AF2, ccmr2_output, oc3m, oc3pe);
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH3, output_to_pa12, PA12, AF2, ccmr2_output, oc3m, oc3pe);
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH3, output_to_pc2, PC2, AF2, ccmr2_output, oc3m, oc3pe);
+
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH4, output_to_pa3, PA3, AF2, ccmr2_output, oc4m, oc4pe);
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH4, output_to_pa13, PA13, AF2, ccmr2_output, oc4m, oc4pe);
+        pwm_channel_pin!(WithPins, TIM5, TIM5_CH4, output_to_pc3, PC3, AF2, ccmr2_output, oc4m, oc4pe);
+    }
+}
+
+// TODO: This timer is also present in stm32f378
+#[cfg(any(feature = "stm32f373"))]
+tim5!();
 
 
 // TIM8
