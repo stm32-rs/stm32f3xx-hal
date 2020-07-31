4b89e33342b07a301719b3b1dc7b250ff01f3b2b
Jan Teske <jteske@posteo.net>
2020-07-31 22:26:46 +0200

    Add codegen for GPIO mappings

---
+++ codegen/src/main.rs
@@ -0,0 +1,40 @@
+mod codegen;
+mod cubemx;
+
+use anyhow::Result;
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+
+fn parse_args() -> ArgMatches<'static> {
+    App::new("codegen")
+        .about("Code generation for the stm32f3xx-hal crate")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("gpio")
+                .about("Generate GPIO mappings from an STM32CubeMX database")
+                .arg(
+                    Arg::with_name("db_path")
+                        .help("Path of the STM32CubeMX MCU database (.../db/mcs/)")
+                        .required(true),
+                ),
+        )
+        .get_matches()
+}
+
+fn main() -> Result<()> {
+    let args = parse_args();
+
+    match args.subcommand() {
+        ("gpio", Some(args)) => handle_gpio(args),
+        _ => unreachable!(),
+    }
+}
+
+fn handle_gpio(args: &ArgMatches) -> Result<()> {
+    let db_path = args.value_of("db_path").unwrap();
+    let db = cubemx::Db::new(db_path);
+
+    let gpio_ips = cubemx::load_f3_gpio_ips(&db)?;
+    codegen::gpio::gen_mappings(&gpio_ips)?;
+
+    Ok(())
+}
