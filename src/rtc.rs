1cb53a83132882848f69d30f157096bf9c2ea100
David O'Connor <david.alan.oconnor@gmail.com>
2020-09-02 22:04:56 -0400

    Travis fix

--- src/rtc.rs
+++ src/rtc.rs
@@ -363,7 +363,6 @@ fn hours_to_register(hours: Hours) -> Result<(u8, u8), Error> {
     match hours {
         Hours::H24(h @ 0..=23) => Ok(bcd2_encode(h as u32)),
         Hours::AM(h @ 1..=12) | Hours::AM(h @ 1..=12) => Ok(bcd2_encode(h as u32)),
-        _ => Err(Error::InvalidInputData),
     }
 }
 
