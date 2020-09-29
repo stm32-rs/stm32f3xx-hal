b64efb2bbe8074f2c9888c9894b1e93338fcb120
David O'Connor <david.alan.oconnor@gmail.com>
2020-09-29 17:47:02 -0400

    fixed missing lines

--- examples/serial.rs
+++ examples/serial.rs
@@ -20,6 +20,11 @@ fn main() -> ! {
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+    // Setup UART pins.
+    let uart_pins = (
+        gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
+        gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
+    );
 
     let serial = Serial::usart1(dp.USART1, uart_pins, 9600.bps(), clocks, &mut rcc.apb2);
     let (mut tx, mut rx) = serial.split();
