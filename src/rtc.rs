8f177bb6b5790952cbae218cc2076b2099fcfa64
David O'Connor <david.alan.oconnor@gmail.com>
2021-01-02 18:20:05 -0500

    update set_update_interval too

--- src/rtc.rs
+++ src/rtc.rs
@@ -205,13 +205,12 @@ impl Rtc {
         // on sleep time. If in the overlap area, choose the lower (more precise) mode.
         // These all assume a 1hz `ck_spre`.
 
-        let lfe_freq = 32_768;
+        let lfe_freq = 32_768.;
 
         // sleep_time = (1/lfe_freq) * div * wutr
         // res = 1/lfe_freq * div
         // sleep_time = res * WUTR = 1/lfe_freq * div * (wutr + 1)
         // wutr = sleep_time * lfe_freq / div - 1
-
 
         let clock_cfg;
         let wutr;
@@ -308,7 +307,8 @@ impl Rtc {
     #[cfg(any(feature = "rt"))]
     #[cfg(any(feature = "stm32f302", feature = "stm32f303"))]
     /// Change the sleep time for the auto wakeup, after it's been set up.
-    pub fn set_wakeup_interval(&mut self, sleep_time: u32) {
+    /// Sleep time is in MS. Major DRY from `set_wakeup`.
+    pub fn set_wakeup_interval(&mut self, sleep_time: f32) {
         // See comments in `set_auto_wakeup` for what these writes do.
         self.regs.wpr.write(|w| unsafe { w.bits(0xCA) });
         self.regs.wpr.write(|w| unsafe { w.bits(0x53) });
@@ -316,12 +316,90 @@ impl Rtc {
         self.regs.cr.modify(|_, w| w.wute().clear_bit());
         while !self.regs.isr.read().wutwf().bit_is_set() {}
 
-        let lfe_freq = 32_768;
-        let sleep_for_cycles = lfe_freq * sleep_time / 1_000;
-        self.regs
-            .wutr
-            .modify(|_, w| w.wut().bits(sleep_for_cycles as u16));
+        let lfe_freq = 32_768.;
 
+        // sleep_time = (1/lfe_freq) * div * wutr
+        // res = 1/lfe_freq * div
+        // sleep_time = res * WUTR = 1/lfe_freq * div * (wutr + 1)
+        // wutr = sleep_time * lfe_freq / div - 1
+
+        let clock_cfg;
+        let wutr;
+
+        // todo: QC this logic. I think the lower bounds on `portion_through` aren't
+        // todo quite right (Maybe they should be 0?) But it doesn't matter.
+        if sleep_time > 0.12207 && sleep_time < 32_000. {
+            let division;
+            let div;
+            if sleep_time < 4_000. {
+                division = WakeupDivision::Two; // Resolution: 61.035µs
+                div = 2.;
+            } else if sleep_time < 8_000. {
+                division = WakeupDivision::Four; // Resolution: 122.08µs
+                div = 4.;
+            } else if sleep_time < 16_000. {
+                division = WakeupDivision::Eight; // Resolution: 244.141
+                div = 8.;
+            } else {
+                division = WakeupDivision::Sixteen; // Resolution: 488.281
+                div = 16.;
+            }
+            clock_cfg = ClockConfig::One(division);
+            wutr = sleep_time / 1_000. * lfe_freq / div - 1.
+        } else if sleep_time < (65_536. * 1_000.) {
+            // 32s to 18 hours
+            clock_cfg = ClockConfig::Two;
+            // todo: Is `portion_through` the right approach?
+            wutr = portion_through(sleep_time, 0., 65_536. * 1_000.);
+        } else if sleep_time < (131_072. * 1_000.) {
+            // 18 to 36 hours
+            clock_cfg = ClockConfig::Three;
+            wutr = portion_through(sleep_time, 65_536. * 1_000., 131_072. * 1_000.);
+        } else {
+            panic!("Wakeup period must be between 0122.07µs and 36 hours.")
+        }
+
+        self.regs.wutr.modify(|_, w| w.wut().bits(wutr as u16));
+
+        // Select the desired clock source. Program WUCKSEL[2:0] bits in RTC_CR register.
+        // See ref man Section 2.4.2: Maximum and minimum RTC wakeup period.
+        // todo: Check register docs and see what to set here.
+
+        // See AN4759, Table 13. RM, 27.3.6
+
+        // When ck_spre frequency is 1Hz, this allows to achieve a wakeup time from 1 s to
+        // around 36 hours with one-second resolution. This large programmable time range is
+        // divided in 2 parts:
+        // – from 1s to 18 hours when WUCKSEL [2:1] = 10
+        // – and from around 18h to 36h when WUCKSEL[2:1] = 11. In this last case 216 is
+        // added to the 16-bit counter current value.When the initialization sequence is
+        // complete (see Programming the wakeup timer on page 781), the timer starts
+        // counting down.When the wakeup function is enabled, the down-counting remains
+        // active in low-power modes. In addition, when it reaches 0, the WUTF flag is set in
+        // the RTC_ISR register, and the wakeup counter is automatically reloaded with its
+        // reload value (RTC_WUTR register value).
+        let word = match clock_cfg {
+            ClockConfig::One(division) => match division {
+                WakeupDivision::Sixteen => 0b000,
+                WakeupDivision::Eight => 0b001,
+                WakeupDivision::Four => 0b010,
+                WakeupDivision::Two => 0b011,
+            },
+            // for 2 and 3, what does `x` mean in the docs? Best guess is it doesn't matter.
+            ClockConfig::Two => 0b100,   // eg 1s to 18h.
+            ClockConfig::Three => 0b110, // eg 18h to 36h
+        };
+
+        // 000: RTC/16 clock is selected
+        // 001: RTC/8 clock is selected
+        // 010: RTC/4 clock is selected
+        // 011: RTC/2 clock is selected
+        // 10x: ck_spre (usually 1 Hz) clock is selected
+        // 11x: ck_spre (usually 1 Hz) clock is selected and 216 is added to the WUT counter value
+        self.regs
+            .cr
+            .modify(|_, w| unsafe { w.wucksel().bits(word) });
+        // end dry.
         self.regs.cr.modify(|_, w| w.wute().set_bit());
         self.regs.wpr.write(|w| unsafe { w.bits(0xFF) });
     }
