51458d6bdb71e9d54ef9fc05f5e4485a6060fe77
seanybaggins <seli2854@colorado.edu>
2021-01-29 20:09:03 +0000

    stuff is compiling

--- gpio_macros/src/lib.rs
+++ gpio_macros/src/lib.rs
@@ -214,6 +214,7 @@ pub fn gpio_pin(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         alternate_functions,
     } = parse_macro_input!(input as GpioPinParsedInfo);
 
+    let GPIOI = format_ident!("GPIO{}", port);
     let PIi = format_ident!("P{}{}", port, pin);
     let PIx = format_ident!("P{}x", port);
     let moderi = format_ident!("moder{}", pin);
@@ -235,13 +236,23 @@ pub fn gpio_pin(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         .iter()
         .map(|alternate_function| format_ident!("AF{}", alternate_function.base10_digits()));
 
+    let PIi_AFis_doc = AFis.clone().map(|AFi| {
+        format!(
+            "Configures `{}` to serve as alternate function: `{}`",
+            PIi.clone(),
+            AFi
+        )
+    });
+
+    let PIi_doc = format!("Pin `{}`", PIi.clone());
+
     let expanded = quote! {
-        ///Pin `#PIi`
+        #[doc = #PIi_doc]
         pub struct #PIi<MODE> {
             _mode: PhantomData<MODE>,
         }
         impl<MODE> #PIi<MODE> {
-            #(///Configures `#PIi` to serve as alternate function: `#AFis`
+            #(#[doc = #PIi_AFis_doc]
             pub fn #into_afis(self, moder: &mut MODER, afr: &mut AFRL) -> #PIi<#AFis> {
                 moder.moder().modify(|_, w| w.#moderi().alternate());
                 afr.afr().modify(|_, w| w.#afrli().#afis());
@@ -360,7 +371,7 @@ pub fn gpio_pin(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 Ok(!self.is_low()?)
             }
             fn is_low(&self) -> Result<bool, Self::Error> {
-                Ok(unsafe { (*GPIOB::ptr()).idr.read().#idri().is_low() })
+                Ok(unsafe { (*#GPIOI::ptr()).idr.read().#idri().is_low() })
             }
         }
         #[cfg(feature = "unproven")]
@@ -370,7 +381,7 @@ pub fn gpio_pin(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 Ok(!self.is_low()?)
             }
             fn is_low(&self) -> Result<bool, Self::Error> {
-                Ok(unsafe { (*GPIOB::ptr()).idr.read().#idri().is_low() })
+                Ok(unsafe { (*#GPIOI::ptr()).idr.read().#idri().is_low() })
             }
         }
         #[cfg(feature = "unproven")]
@@ -379,7 +390,7 @@ pub fn gpio_pin(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 self.is_set_low().map(|b| !b)
             }
             fn is_set_low(&self) -> Result<bool, Self::Error> {
-                Ok(unsafe { (*GPIOB::ptr()).odr.read().#odri().is_low() })
+                Ok(unsafe { (*#GPIOI::ptr()).odr.read().#odri().is_low() })
             }
         }
         #[cfg(feature = "unproven")]
@@ -387,6 +398,250 @@ pub fn gpio_pin(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     };
 
     eprintln!("{}", expanded);
+
+    proc_macro::TokenStream::from(expanded)
+}
+
+/// Holds relevant information that is parsed from the gpio! macro invocation
+struct GpioParsedInfo {
+    port_lower: String,
+    port_upper: String,
+    pins_to_alternate_functions: Vec<(u8, Vec<u8>)>,
+}
+
+impl Parse for GpioParsedInfo {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let _port: Ident = input.parse()?;
+        let _collon: Token![:] = input.parse()?;
+        let port: Ident = input.parse()?;
+        let expected_port = Regex::new("^[a-h]$").unwrap();
+        test_input_against_regex(&port.to_string(), &expected_port)?;
+        let port_lower = port.to_string();
+        let port_upper = port.to_string().to_uppercase();
+
+        let _comma: Token![,] = input.parse()?;
+        let _pins_ident: Ident = input.parse()?;
+        let _collon: Token![:] = input.parse()?;
+        let bracketed_content;
+        bracketed!(bracketed_content in input);
+        let pin_to_alternate_functions_parser = |input: ParseStream<'_>| {
+            let pin: u8 = input.parse::<LitInt>()?.base10_parse::<u8>()?;
+
+            let _arrow: Token![=>] = input.parse()?;
+            let _af: Ident = input.parse()?;
+            let _collon: Token![:] = input.parse()?;
+            let alternate_functions;
+            bracketed!(alternate_functions in input);
+            let alternate_functions: Punctuated<LitInt, Token![,]> =
+                Punctuated::parse_separated_nonempty(&alternate_functions)?; // being empty may be fine
+            let alternate_functions: Vec<u8> = alternate_functions
+                .iter()
+                .map(|alternate_function| {
+                    alternate_function
+                        .base10_parse::<u8>()
+                        .expect("unable to parse an integer for an alternate function")
+                })
+                .collect();
+
+            Ok((pin, alternate_functions))
+        };
+
+        let pins_to_alternate_functions: Punctuated<(u8, Vec<u8>), Token![,]> =
+            Punctuated::parse_separated_nonempty_with(
+                &bracketed_content,
+                pin_to_alternate_functions_parser,
+            )?;
+
+        //let pins_to_alternate_functions: syn::ExprArray = input.parse()?;
+        let pins_to_alternate_functions: Vec<(u8, Vec<u8>)> =
+            pins_to_alternate_functions.iter().cloned().collect();
+
+        Ok(Self {
+            port_lower,
+            port_upper,
+            pins_to_alternate_functions,
+        })
+    }
+}
+
+/// Creates the structs and implementations of gpio pins
+/// # Usage
+/// ```rust
+/// gpio!(port: a, pins: [
+///    0 => af: [1, 3, 7, 8, 9, 10, 15],
+///    1 => af: [0, 1, 3, 7, 9, 15],
+///    2 => af: [1, 3, 7, 8, 9, 15],
+///    3 => af: [1, 3, 7, 9, 15],
+///    4 => af: [2, 3, 5, 6, 7, 15],
+///    5 => af: [1, 3, 5, 15],
+///    6 => af: [1, 2, 3, 4, 5, 6, 8, 15],
+///    7 => af: [1, 2, 3, 4, 5, 6, 8, 15],
+///    8 => af: [0, 4, 5, 6, 7, 8, 10, 15],
+///    9 => af: [3, 4, 5, 6, 7, 8, 9, 10, 15],
+///    10 => af: [1, 3, 4, 6, 7, 8, 10, 11, 15],
+///    11 => af: [6, 7, 8, 9, 10, 11, 12, 14, 15],
+///    12 => af: [1, 6, 7, 8, 9, 10, 11, 14, 15],
+///    13 => af: [0, 1, 3, 5, 7, 10, 15],
+///    14 => af: [0, 3, 4, 5, 6, 7, 15],
+///    15 => af: [0, 1, 2, 4, 5, 6, 7, 9, 15],
+///])
+/// ```
+#[proc_macro]
+pub fn gpio(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let GpioParsedInfo {
+        port_lower,
+        port_upper,
+        pins_to_alternate_functions,
+    } = parse_macro_input!(input as GpioParsedInfo);
+
+    let GPIOI = format_ident!("GPIO{}", port_upper);
+    let gpioi = format_ident!("gpio{}", port_lower);
+    let pini: Vec<u8> = pins_to_alternate_functions
+        .iter()
+        .cloned()
+        .map(|(pin, _alternate_functions)| pin)
+        .collect();
+    let alternate_functionsi = pins_to_alternate_functions
+        .iter()
+        .map(|(_pin, alternate_functions)| alternate_functions);
+    let PIx = format_ident!("P{}x", port_upper);
+    let PIis: Vec<Ident> = pini
+        .iter()
+        .map(|pin| format_ident!("P{}{}", port_upper, pin))
+        .collect();
+    let piis : Vec<Ident> = pini
+        .iter()
+        .map(|pin| format_ident!("p{}{}", port_lower, pin))
+        .collect();
+    // let AFIs = alternate_functionsi.clone().map(|alternate_functions| {
+    //     alternate_functions
+    //         .iter()
+    //         .map(|alternate_function| format_ident!("AF{}", alternate_function))
+    // });
+    let resets = pini.iter().map(|pin| match (port_lower.as_str(), pin) {
+        ("a", _pin @ 13..=15) => format_ident!("AF0"),
+        ("b", _pin @ 3..=4) => format_ident!("AF0"),
+        _ => format_ident!("INPUT<FLOATING>"),
+    });
+    let iopien = format_ident!("iop{}en", port_lower);
+    let iopirst = format_ident!("iop{}rst", port_lower);
+
+    // Is this a bug?
+    let gpio_mapped = match port_lower.as_str() {
+        "a" => format_ident!("gpioa"),
+        "b" => format_ident!("gpiob"),
+        _ => format_ident!("gpioc"),
+        // What about the mapping for gpio-f373?
+        // Should the alternate mapping be d -> d?
+        // This is contrary to every other board.
+    };
+
+    let expanded = quote! {
+        /// "All Pins and associated functions for GPIO Bank: `GPIOA`
+        pub mod #gpioi {
+            use core::marker::PhantomData;
+            use core::convert::Infallible;
+            use crate::hal::digital::v2::OutputPin;
+            #[cfg(feature = "unproven")]
+            use crate::hal::digital::v2::InputPin;
+            #[cfg(feature = "unproven")]
+            use crate::hal::digital::v2::StatefulOutputPin;
+            #[cfg(feature = "unproven")]
+            use crate::hal::digital::v2::toggleable;
+            use crate::pac::{#gpioi, #GPIOI};
+            use crate::rcc::AHB;
+            #[allow(unused_imports)]
+            use super::{
+                AF0, AF1, AF2, AF3, AF4, AF5, AF6, AF7, AF8, AF9, AF10, AF11, AF12, AF13, AF14, AF15,
+            };
+            use super::{
+                Floating, GpioExt, Input, OpenDrain, Output, Analog, PullDown, PullUp, PushPull, PXx,
+                Gpio,
+            };
+            /// " GPIO parts
+            pub struct Parts {
+                /// " Opaque AFRH register
+                pub afrh: AFRH,
+                /// " Opaque AFRL register
+                pub afrl: AFRL,
+                /// " Opaque MODER register
+                pub moder: MODER,
+                /// " Opaque OTYPER register
+                pub otyper: OTYPER,
+                /// " Opaque PUPDR register
+                pub pupdr: PUPDR,
+                /// Pin
+                #(pub #piis: #PIis<#resets>),*
+
+            }
+            impl GpioExt for #GPIOI {
+                type Parts = Parts;
+                fn split(self, ahb: &mut AHB) -> Parts {
+                    ahb.enr().modify(|_, w| w.#iopien().set_bit());
+                    ahb.rstr().modify(|_, w| w.#iopirst().set_bit());
+                    ahb.rstr().modify(|_, w| w.#iopirst().clear_bit());
+                    Parts {
+                        afrh: AFRH { _0: () },
+                        afrl: AFRL { _0: () },
+                        moder: MODER { _0: () },
+                        otyper: OTYPER { _0: () },
+                        pupdr: PUPDR { _0: () },
+                        #(#piis: #PIis { _mode: PhantomData }),*
+                    }
+                }
+            }
+            /// " Opaque AFRL register
+            pub struct AFRL {
+                _0: (),
+            }
+            impl AFRL {
+                #[allow(dead_code)]
+                pub(crate) fn afr(&mut self) -> &#gpio_mapped::AFRL {
+                    unsafe { &(*#GPIOI::ptr()).afrl }
+                }
+            }
+            /// " Opaque AFRH register
+            pub struct AFRH {
+                _0: (),
+            }
+            impl AFRH {
+                #[allow(dead_code)]
+                pub(crate) fn afr(&mut self) -> &gpioa::AFRH {
+                    unsafe { &(*#GPIOI::ptr()).afrh }
+                }
+            }
+            /// " Opaque MODER register
+            pub struct MODER {
+                _0: (),
+            }
+            impl MODER {
+                pub(crate) fn moder(&mut self) -> &gpioa::MODER {
+                    unsafe { &(*#GPIOI::ptr()).moder }
+                }
+            }
+            /// " Opaque OTYPER register
+            pub struct OTYPER {
+                _0: (),
+            }
+            impl OTYPER {
+                pub(crate) fn otyper(&mut self) -> &#gpio_mapped::OTYPER {
+                    unsafe { &(*#GPIOI::ptr()).otyper }
+                }
+            }
+            /// " Opaque PUPDR register
+            pub struct PUPDR {
+                _0: (),
+            }
+            impl PUPDR {
+                pub(crate) fn pupdr(&mut self) -> &#gpio_mapped::PUPDR {
+                    unsafe { &(*#GPIOI::ptr()).pupdr }
+                }
+            }
+
+            gpio_partially_erased!(#PIx);
+
+        }
+    };
 
     proc_macro::TokenStream::from(expanded)
 }
