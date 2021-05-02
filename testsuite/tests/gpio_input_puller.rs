20bf75c806cedc0b67f1eb1b7fe16b65a8bbca4a
Sh3Rm4n <f.vioel@googlemail.com>
2021-05-02 15:51:17 +0200

    Add pull up and down test

---
+++ testsuite/tests/gpio_input_puller.rs
@@ -0,0 +1,70 @@
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _;
+use panic_probe as _;
+
+use stm32f3xx_hal as hal;
+
+use hal::gpio::gpioc;
+use hal::gpio::Resistor;
+use hal::gpio::{Input, PXx};
+use hal::{pac, prelude::*};
+
+struct State {
+    observer: PXx<Input>,
+    puller: gpioc::PC1<Input>,
+    pupdr: gpioc::PUPDR,
+}
+
+#[defmt_test::tests]
+mod tests {
+    use super::*;
+    use defmt::{assert, unwrap};
+
+    // Test the defaults with no configuration
+    #[init]
+    fn init() -> super::State {
+        let dp = unwrap!(pac::Peripherals::take());
+
+        let mut rcc = dp.RCC.constrain();
+        let mut gpioc = dp.GPIOC.split(&mut rcc.ahb);
+
+        let mut observer = gpioc
+            .pc0
+            .into_floating_input(&mut gpioc.moder, &mut gpioc.pupdr);
+        observer.set_internal_resistor(&mut gpioc.pupdr, Resistor::Floating);
+
+        let observer = observer.downgrade().downgrade();
+
+        let puller = gpioc
+            .pc1
+            .into_floating_input(&mut gpioc.moder, &mut gpioc.pupdr);
+
+        let pupdr = gpioc.pupdr;
+
+        super::State {
+            observer,
+            puller,
+            pupdr,
+        }
+    }
+
+    #[test]
+    fn pulldown_is_low(state: &mut super::State) {
+        state
+            .puller
+            .set_internal_resistor(&mut state.pupdr, Resistor::PullDown);
+        assert!(unwrap!(state.puller.is_low()));
+        assert!(unwrap!(state.observer.is_low()));
+    }
+
+    #[test]
+    fn set_high_is_high(state: &mut super::State) {
+        state
+            .puller
+            .set_internal_resistor(&mut state.pupdr, Resistor::PullUp);
+        assert!(unwrap!(state.puller.is_high()));
+        assert!(unwrap!(state.observer.is_high()));
+    }
+}
