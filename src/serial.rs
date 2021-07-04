eefe6ec70cb5d255932ffb2177c6c316faaf82e5
Sh3Rm4n <f.vioel@googlemail.com>
2021-07-04 22:01:47 +0200

    Improve documentation

--- src/serial.rs
+++ src/serial.rs
@@ -116,6 +116,9 @@ cfg_if! {
 }
 
 /// Serial abstraction
+///
+/// This is an abstraction of the UART peripheral intended to be
+/// used for standard duplex serial communication.
 pub struct Serial<Usart, Pins> {
     usart: Usart,
     pins: Pins,
