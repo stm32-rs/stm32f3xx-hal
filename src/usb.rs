f8b71b027262d902c520427393f0694933be5b22
Russell McClellan <russell.mcclellan@gmail.com>
2021-07-13 22:53:59 -0400

    Fixes from review

--- src/usb.rs
+++ src/usb.rs
@@ -11,44 +11,44 @@
 use crate::pac::{RCC, USB};
 use stm32_usbd::UsbPeripheral;
 
+use crate::gpio;
 use crate::gpio::gpioa::{PA11, PA12};
-use crate::gpio::{PushPull, AF14};
 pub use stm32_usbd::UsbBus;
 
 /// Trait implemented by all pins that can be the "D-" pin for the USB peripheral
-pub trait DMPin: crate::private::Sealed {}
+pub trait DmPin: crate::private::Sealed {}
 
 /// Trait implemented by all pins that can be the "D+" pin for the USB peripheral
-pub trait DPPin: crate::private::Sealed {}
+pub trait DpPin: crate::private::Sealed {}
 
 #[cfg(any(feature = "stm32f303xb", feature = "stm32f303xc"))]
-impl DMPin for PA11<AF14<PushPull>> {}
+impl DmPin for PA11<gpio::AF14<gpio::PushPull>> {}
 
 #[cfg(any(feature = "stm32f303xb", feature = "stm32f303xc"))]
-impl DPPin for PA12<AF14<PushPull>> {}
+impl DpPin for PA12<gpio::AF14<gpio::PushPull>> {}
 
 #[cfg(any(feature = "stm32f303xd", feature = "stm32f303xe"))]
-impl<Mode> DMPin for PA11<Mode> {}
+impl<Mode> DmPin for PA11<Mode> {}
 
 #[cfg(any(feature = "stm32f303xd", feature = "stm32f303xe"))]
-impl<Mode> DPPin for PA12<Mode> {}
+impl<Mode> DpPin for PA12<Mode> {}
 
 /// USB Peripheral
 ///
 /// Constructs the peripheral, which
 /// than gets passed to the [`UsbBus`].
-pub struct Peripheral<DM: DMPin, DP: DPPin> {
+pub struct Peripheral<Dm: DmPin, Dp: DpPin> {
     /// USB Register Block
     pub usb: USB,
     /// Data Negativ Pin
-    pub pin_dm: DM,
+    pub pin_dm: Dm,
     /// Data Positiv Pin
-    pub pin_dp: DP,
+    pub pin_dp: Dp,
 }
 
-unsafe impl<DM: DMPin, DP: DPPin> Sync for Peripheral<DM, DP> {}
+unsafe impl<Dm: DmPin, Dp: DpPin> Sync for Peripheral<Dm, Dp> {}
 
-unsafe impl<DM: DMPin + Send, DP: DPPin + Send> UsbPeripheral for Peripheral<DM, DP> {
+unsafe impl<Dm: DmPin + Send, Dp: DpPin + Send> UsbPeripheral for Peripheral<Dm, Dp> {
     const REGISTERS: *const () = USB::ptr() as *const ();
     const DP_PULL_UP_FEATURE: bool = false;
     const EP_MEMORY: *const () = 0x4000_6000 as _;
@@ -83,8 +83,13 @@ unsafe impl<DM: DMPin + Send, DP: DPPin + Send> UsbPeripheral for Peripheral<DM,
 }
 
 /// Type of the UsbBus
-///
-/// As this MCU family has only USB peripheral,
-/// this is the only possible concrete type construction.
-pub type UsbBusType<DP = PA11<AF14<PushPull>>, DM = PA12<AF14<PushPull>>> =
-    UsbBus<Peripheral<DM, DP>>;
+#[cfg(any(feature = "stm32f303xb", feature = "stm32f303xc"))]
+pub type UsbBusType<Dm = PA11<gpio::AF14<gpio::PushPull>>, Dp = PA12<gpio::AF14<gpio::PushPull>>> =
+    UsbBus<Peripheral<Dm, Dp>>;
+
+/// Type of the UsbBus
+#[cfg(any(feature = "stm32f303xd", feature = "stm32f303xe"))]
+pub type UsbBusType<
+    Dm = PA11<gpio::Output<gpio::PushPull>>,
+    Dp = PA12<gpio::Output<gpio::PushPull>>,
+> = UsbBus<Peripheral<Dm, Dp>>;
