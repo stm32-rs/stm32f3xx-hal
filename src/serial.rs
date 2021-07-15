0823ab7b0972f70cb1eab1d84a8eabf0dd18860b
Sh3Rm4n <f.vioel@googlemail.com>
2021-07-15 09:07:05 +0200

    Make Event enum copy

--- src/serial.rs
+++ src/serial.rs
@@ -28,7 +28,7 @@ use cortex_m::interrupt;
 
 /// Interrupt and status events
 // TODO: Sort as flags are ordered in register
-#[derive(Debug, EnumSetType)]
+#[derive(Copy, Debug, EnumSetType)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[non_exhaustive]
 pub enum Event {
