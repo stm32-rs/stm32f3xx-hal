5a26113f19a0c512b5b96a0d1b917674ff8c1821
Sh3Rm4n <f.vioel@googlemail.com>
2021-07-31 17:38:28 +0200

    Add basic usage to documentation root

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,35 @@
 
  [stm]: https://www.st.com/en/microcontrollers-microprocessors/stm32f3-series.html
 
+ ## Basic Usagee
+
+ ```rust
+ #![no_std]
+ #![no_main]
+
+ use cortex_m::asm;
+ use cortex_m_rt::entry;
+ use panic_halt as _;
+ use stm32f3xx_hal::{self as hal, pac, prelude::*};
+
+ #[entry]
+ fn main() -> ! {
+       let dp = pac::Peripherals::take().unwrap();
+
+       let mut rcc = dp.RCC.constrain();
+       let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+
+       let mut led = gpioe
+             .pe13
+             .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+       loop {
+             led.toggle().unwrap();
+             asm::delay(8_000_000);
+       }
+ }
+ ```
+
  ## Cargo features
 
  ### Target chip selection
